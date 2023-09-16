f977f193d2c34eb8af589c97b921ce188ea4aae0
Milind Jha <75353076+Milind-Jha@users.noreply.github.com>
2023-09-16 21:08:01 +0530

    Update LoanScheduler.java

--- loan-management-app/src/main/java/com/example/loanstore/scheduler/LoanScheduler.java
+++ loan-management-app/src/main/java/com/example/loanstore/scheduler/LoanScheduler.java
@@ -20,15 +20,15 @@ public class LoanScheduler implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // Add loans
-//        Loan loan1 = new Loan("L1", "C1", "LEN1", 10000, 10000, LocalDate.of(2023, 6, 5), 1, LocalDate.of(2023, 7, 5), 0.01);
-//        Loan loan2 = new Loan("L2", "C1", "LEN1", 20000, 5000, LocalDate.of(2023, 6, 1), 1, LocalDate.of(2023, 8, 5), 0.01);
-//        Loan loan3 = new Loan("L3", "C2", "LEN2", 50000, 30000, LocalDate.of(2023, 4, 4), 2, LocalDate.of(2023, 5, 4), 0.02);
-//        Loan loan4 = new Loan("L4", "C3", "LEN2", 50000, 30000, LocalDate.of(2023, 4, 4), 2, LocalDate.of(2023, 5, 4), 0.02);
-//
-//        loanRepository.save(loan1);
-//        loanRepository.save(loan2);
-//        loanRepository.save(loan3);
-//        loanRepository.save(loan4);
+        Loan loan1 = new Loan("L1", "C1", "LEN1", 10000, 10000, LocalDate.of(2023, 6, 5), 1, LocalDate.of(2023, 7, 5), 0.01);
+        Loan loan2 = new Loan("L2", "C1", "LEN1", 20000, 5000, LocalDate.of(2023, 6, 1), 1, LocalDate.of(2023, 8, 5), 0.01);
+        Loan loan3 = new Loan("L3", "C2", "LEN2", 50000, 30000, LocalDate.of(2023, 4, 4), 2, LocalDate.of(2023, 5, 4), 0.02);
+        Loan loan4 = new Loan("L4", "C3", "LEN2", 50000, 30000, LocalDate.of(2023, 4, 4), 2, LocalDate.of(2023, 5, 4), 0.02);
+
+        loanRepository.save(loan1);
+        loanRepository.save(loan2);
+        loanRepository.save(loan3);
+        loanRepository.save(loan4);
     }
 
     @Scheduled(cron = "* * * * * *")
